38cd4f5ad1d53360491caa63d352273ed9639188
Duncanwar <semunda9@gmail.com>
2021-07-26 20:44:54 +0200

    backend calls http app

---
+++ http-app/src/services/logService.js
@@ -0,0 +1,23 @@
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+function init() {
+  Sentry.init({
+    dsn: "https://aecbfc6e402349aca26a9fd8a0344653@o931235.ingest.sentry.io/5880123",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+}
+
+function log(error) {
+  Sentry.captureException(error);
+}
+
+export default {
+  init,
+  log,
+};
