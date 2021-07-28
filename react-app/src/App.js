abf15786ae3894296a48b3f4f07b2ccb387149d4
Duncanwar <semunda9@gmail.com>
2021-07-28 16:49:27 +0200

    updating context

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
+import Login from "./context/Login";
 import MoviePage from "./context/MoviePage";
 import UserContext from "./context/userContext";
 
 class App extends Component {
-  state = { currentUser: { name: "Mosh" } };
+  handleLoggedIn = (username) => {
+    console.log("Getting the user:" + username);
+    const user = { name: "Duncan" };
+    this.setState({ currentUser: user });
+  };
+  state = { currentUser: null };
 
   render() {
     return (
-      <UserContext.Provider value={this.state.currentUser}>
+      <UserContext.Provider
+        value={{
+          currentUser: this.state.currentUser,
+          onLoggedIn: this.handleLoggedIn,
+        }}
+      >
         <div>
           <MoviePage />
         </div>
+        <Login />
       </UserContext.Provider>
     );
   }
