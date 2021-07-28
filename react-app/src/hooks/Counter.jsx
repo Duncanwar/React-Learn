257a4ee7c05856a6a3b9e93785de87ecc0d038c1
Duncanwar <semunda9@gmail.com>
2021-07-28 15:52:07 +0200

    custom hook useEffect

--- react-app/src/hooks/Counter.jsx
+++ react-app/src/hooks/Counter.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
+import useDocumentTitle from "./useDocumentTitle";
 
 function Counter(props) {
   const [count, setCount] = useState(0);
   const [name, setName] = useState("");
-
+  useDocumentTitle(`${name} has clicked ${count} times`);
   return (
     <>
       <input type="text" onChange={(e) => setName(e.target.value)} />
